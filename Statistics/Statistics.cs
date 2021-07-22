52c03653835836b515530f5da9fd9ec47cf38f79
vrunda13 <43112048+vrunda13@users.noreply.github.com>
2021-07-22 16:23:27 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,8 +5,31 @@ namespace Statistics
 {
     public class StatsComputer
     {
+        public double average = 0.0;
+        public float min = float.MaxValue;
+        public float max = float.MinValue;
+        
         public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+            Stats numbers = new Stats();
+            float sumOfValues = 0;
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                sumOfValues += numbers[i];
+                if(numbers[i] > max){
+                    max = numbers[i];
+                }
+                if(numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+                
+            }
+      
+            averageValue = sumOfValues/numbers.Count;
+            numbers.average = average;
+            numbers.max = max;
+            numbers.min = min;
+            return Values;
             
         }
     }
