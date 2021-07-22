9cfd098e21d7b41435dd26480065436bc87a0637
vrunda13 <43112048+vrunda13@users.noreply.github.com>
2021-07-22 16:38:58 +0530

    Create StatsAlerter.cs

---
+++ Statistics/StatsAlerter.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistics
+{
+    public class StatsAlerter
+    {
+        float maxthreshold;
+        IAlerter[] alerters;
+
+        public StatsAlerter(float maxthreshold, IAlerter[] alerters)
+        {
+            this.maxthreshold = maxthreshold;
+            this.alerters = alerters;
+        }
+
+        public void checkAndAlert(List<float> thresholds)
+        {
+            for(int i = 0; i < thresholds.Count; ++i)
+            {
+                if(thresholds[i] > maxThreshold)
+                {
+                    for(int j = 0; j < alerter.Length; ++j)
+                    {
+                        alerter[j].work();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
